8fb2be83e8e48235454f58a9d3d1bd251d9f9dce
Mahrab hasan Niloy <173291146+Niloy441@users.noreply.github.com>
2026-05-12 08:26:09 +0600

    Set up Xvfb and x11vnc in nxserver.sh

--- docker/nxserver.sh
+++ docker/nxserver.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "ok"
+Xvfb :1 -screen 0 1600x900x24 &
+sleep 2
+DISPLAY=:1 startxfce4 &
+sleep 3
+x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -loop -noxdamage -repeat -rfbport 5900 &
+websockify --web /usr/share/novnc 8080 localhost:5900
